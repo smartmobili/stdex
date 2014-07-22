0a369b235a4d0fc17addaa843c829ef0a48954a6
Vincent R. <v.richomme@gmail.com>
2014-07-23 00:33:41 +0200

    first import

---
+++ test_stdex.cpp
@@ -0,0 +1,24 @@
+
+#include <cstdio>
+#include <cstdtchar>
+#include <string>
+#include <stringex>
+
+using namespace std;
+using namespace stdex;
+
+int _tmain(int argc, tchar_t* argv[])
+{
+	string sStdTest0;
+	string sStdTest1("");
+
+	stringex sTest0;
+	stringex sTest1("");
+	stringex sTest2(L"");
+	stringex sTest3(L"Hello zobby");
+
+	tstringex sTest = "Test";
+
+	return 0;
+}
+
